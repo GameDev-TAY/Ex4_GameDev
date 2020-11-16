ec8afe9c10bcadcd5f9e3809ae94d61e739c6146
Ariel Duek <arielduek@gmail.com>
2020-11-16 17:34:01 +0200

    Orginal + Some Order

---
+++ Assets/Scripts/1-movers/Chaser.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chaser : MonoBehaviour
+{
+    Transform Player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Player==null){
+            GameObject Go = GameObject.Find("PlayerSpaceship");
+            if (Go != null){
+                Player = Go.transform;
+            }
+        }
+
+        if (Player == null) return;
+
+        Vector3 dir = Player.position - transform.position;
+        dir.Normalize();
+
+        float zAngle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
+    }
+}
