f63d680dc34976487e91d5c44a2370bd4bac0b84
Ariel Duek <arielduek@gmail.com>
2020-11-17 14:42:14 +0200

    Walls Spawner added

---
+++ Assets/Scripts/Boundaries/SpawnRandomWalls.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnRandomWalls : MonoBehaviour
+{
+    [SerializeField] GameObject prefabToSpawn;
+    [Tooltip("Minimum time between consecutive spawns, in seconds")] [SerializeField] float minTimeBetweenSpawns = 5f;
+    [Tooltip("Maximum time between consecutive spawns, in seconds")] [SerializeField] float maxTimeBetweenSpawns = 7f;
+    private Vector2 screenBounds;
+
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        this.StartCoroutine(SpawnRoutine());
+    }
+
+    private IEnumerator SpawnRoutine()
+    {
+        while (true)
+        {
+            float timeBetweenSpawns = Random.Range(minTimeBetweenSpawns, maxTimeBetweenSpawns);
+            yield return new WaitForSeconds(timeBetweenSpawns);
+            Vector3 positionOfSpawnedObject = new Vector3(
+                Random.Range(-screenBounds.x, screenBounds.x),
+                Random.Range(-screenBounds.y, screenBounds.y),
+                transform.position.z);
+            GameObject newObject = Instantiate(prefabToSpawn.gameObject, positionOfSpawnedObject, Quaternion.Euler(0f, 0f, Random.Range(0f, 360f)));
+            Destroy(newObject, 5f);
+        }
+    }
+}
